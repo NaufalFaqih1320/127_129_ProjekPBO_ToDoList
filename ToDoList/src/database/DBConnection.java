2105678b12dc87c31751e58087db19133a196f4c
Naufal Faqih <faqih.mustaqim26@gmail.com>
2026-05-26 16:04:35 +0700

    Update DBConnection.java

--- ToDoList/src/database/DBConnection.java
+++ ToDoList/src/database/DBConnection.java
@@ -13,13 +13,19 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
-
+    // URL database MySQL
     private static final String URL = "jdbc:mysql://localhost:3306/todolist_db";
+    
+    // Username database
     private static final String USER = "root";
+    
+    // Password database
     private static final String PASSWORD = "";
 
+    // Method untuk mendapatkan koneksi database
     public static Connection connect() {
         try {
+            // Membuat koneksi ke database menggunakan JDBC
             Class.forName("com.mysql.cj.jdbc.Driver");
             return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (Exception e) {
