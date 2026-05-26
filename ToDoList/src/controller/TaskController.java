4d34cc27126f51e7eb6d25a45e8f757647664b16
Naufal Faqih <faqih.mustaqim26@gmail.com>
2026-05-26 16:03:44 +0700

    Update TaskController.java

--- ToDoList/src/controller/TaskController.java
+++ ToDoList/src/controller/TaskController.java
@@ -12,27 +12,29 @@ import java.util.List;
 import model.Task;
 import model.TaskDAO;
 
+// Class TaskController berfungsi sebagai penghubung antara tampilan (View) dan data (Model).
+// Class ini untuk proses tambah, ambil, update, dan hapus data task.
 public class TaskController {
 
-    TaskDAO dao;
+    private final TaskDAO dao; // Objek DAO digunakan untuk mengakses database
 
-    public TaskController() {
+    public TaskController() { // Constructor untuk membuat objek DAO
         dao = new TaskDAO();
     }
 
-    public void addTask(Task task) {
+    public void addTask(Task task) { // Method untuk menambahkan task baru
         dao.insertTask(task);
     }
 
-    public List<Task> getTasks() {
+    public List<Task> getTasks() { // Method untuk mengambil seluruh data task
         return dao.getAllTasks();
     }
 
-    public void updateTask(Task task) {
+    public void updateTask(Task task) { // Method untuk mengupdate data task
         dao.updateTask(task);
     }
 
-    public void deleteTask(int id) {
+    public void deleteTask(int id) { // Method untuk menghapus task berdasarkan id
         dao.deleteTask(id);
     }
 }
