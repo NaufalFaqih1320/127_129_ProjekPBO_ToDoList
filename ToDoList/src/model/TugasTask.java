811e4b4f38e44f17a1be2d77980fd804025880af
Naufal Faqih <faqih.mustaqim26@gmail.com>
2026-05-26 16:07:52 +0700

    Update TugasTask.java

--- ToDoList/src/model/TugasTask.java
+++ ToDoList/src/model/TugasTask.java
@@ -9,19 +9,28 @@ package model;
  * @author ASUS
  */
 
+// Class TugasTask merupakan subclass dari Task
+// yang digunakan untuk merepresentasikan task jenis tugas
 public class TugasTask extends Task {
-
+    
+    // Constructor untuk mengisi data task tugas
     public TugasTask(int id, String title, String description, String deadline, boolean completed) {
+        // Memanggil constructor parent class Task
+        // untuk menginisialisasi attribute task
         super(id, title, description, deadline, completed);
     }
 
+    // Override method displayInfo dari class Task
     @Override
     public String displayInfo() {
+        // Mengembalikan informasi task dalam bentuk String
         return "Tugas Task : " + title;
     }
 
+    // Override method getType dari class Task
     @Override
     public String getType() {
+        // Mengembalikan tipe task
         return "Tugas";
     }
 }
